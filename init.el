095fc22d0a2a9e03432ce0bf227eec0dbb13b2e3
Noah Hoffman <noah.hoffman@gmail.com>
2022-06-14 08:44:52 -0700

    use :bind for some key bindings

--- init.el
+++ init.el
@@ -415,13 +415,13 @@ Assumes that the frame is only split into two."
 (use-package counsel
   :ensure t
   :pin melpa
+  :bind (("M-x" . counsel-M-x)
+         ("C-x C-f" . counsel-find-file)
+         ("C-c g" . counsel-git)
+         ("C-c j" . counsel-git-grep)
+         ("C-c a" . counsel-ag)
+         ("M-y" . counsel-yank-pop))
   :config
-  (global-set-key (kbd "M-x") 'counsel-M-x)
-  (global-set-key (kbd "C-x C-f") 'counsel-find-file)
-  (global-set-key (kbd "C-c g") 'counsel-git)
-  (global-set-key (kbd "C-c j") 'counsel-git-grep)
-  (global-set-key (kbd "C-c a") 'counsel-ag)
-  (global-set-key (kbd "M-y") 'counsel-yank-pop)
   (define-key minibuffer-local-map (kbd "C-r") 'counsel-minibuffer-history))
 
 (use-package swiper
