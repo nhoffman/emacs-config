815706dc6a988ead5690b85724839323d3b674c5
Noah Hoffman <noah.hoffman@gmail.com>
2024-07-03 09:27:55 -0700

    add tsx-mode

--- init.el
+++ init.el
@@ -891,6 +891,12 @@ selection if no virtualenv is active."
             (make-local-variable 'js-indent-level)
             (setq js-indent-level 2)))
 
+(straight-use-package
+ '(tsx-mode
+   :type git
+   :host github
+   :repo "orzechowskid/tsx-mode.el"))
+
 ;;* ESS (R language support)
 
 (defun nh/set-inferior-ess-r-program-name ()
